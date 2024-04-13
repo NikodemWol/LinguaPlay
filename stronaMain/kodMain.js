b2399e6a352934d97531db9d2351f92c2725defd
Nikodem <wolninikodem@gmail.com>
2024-04-13 12:02:30 +0200

    dodanie ustawien

--- stronaMain/kodMain.js
+++ stronaMain/kodMain.js
@@ -1,9 +1,29 @@
+const muzyka= document.querySelector('#music')
+const x = document.querySelector('.x')
+const ustawieniatlo = document.querySelector('.ustawieniatlo')
+const ustawienia = document.querySelector('.ust')
+const imie= localStorage.getItem('name')
+
+
+function kliknieciex(){
+    ustawieniatlo.classList.add('hidden')
+}
+function klikniecieustawien(){
+    ustawieniatlo.classList.remove('hidden')
+}
+
+
+x.addEventListener('click', kliknieciex)
+ustawienia.addEventListener('click', klikniecieustawien)
+muzyka.play()
+
 const clearJson=document.body.querySelector('.clearJson')
 clearJson.addEventListener('click', function(){
     localStorage.clear()
 })
-const imie= localStorage.getItem('name')
+
 const czesc=document.body.querySelector('.imie')
 const twojeimie=document.body.querySelector('.imie2')
 czesc.innerText='Hi '+ imie+'!'
 twojeimie.innerText=imie.toUpperCase()
+muzyka.volume=1
